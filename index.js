305661cb8f3222db65e7b57c86fa70bb10562103
sugeshc389 <sugeshc389@gmail.com>
2024-04-06 19:46:25 +0530

    res:conflict resolve

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from 'cors'
 
 const app = express();
 dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+// app.use('/api/users')
 
 mongoose.connect(process.env.MONGODB).then(
     console.log('DB Connected')
