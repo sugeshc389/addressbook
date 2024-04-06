f005558804effa0e6a1ed12b613ec1aace40ffc8
sugeshc389 <sugeshc389@gmail.com>
2024-04-06 15:12:52 +0530

    fet: project initialize

---
+++ src/app/users/page.jsx
@@ -0,0 +1,17 @@
+
+const App = () => {
+  return (
+    <div>
+        <h1>User Address Book</h1>
+        <div className="flex flex-col gap-3 justify-center items-center">
+
+        <input type="text" placeholder="Name" />
+        <input type="number" placeholder="Phone Number" />
+        <input type="email" placeholder="Email" />
+        <button></button>
+        </div>
+    </div>
+  )
+}
+
+export default App
