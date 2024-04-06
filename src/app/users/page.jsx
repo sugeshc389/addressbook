5f6e5301b66aca1f632ec57cf2a4702faa2fbb56
sugeshc389 <sugeshc389@gmail.com>
2024-04-06 21:45:56 +0530

    fet:user add

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Button from "../components/Button";
 import newRequest from "../axios/axios";
 
@@ -7,15 +7,17 @@ const App = () => {
   const nameRef = useRef();
   const phoneRef = useRef();
   const emailRef = useRef();
+  const [name, setName] = useState();
+  const [phone, setPhone] = useState();
+  const [email, setEmail] = useState();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const name = nameRef.current.value;
-    const phone = phoneRef.current.value;
-    const email = emailRef.current.value;
+
     try {
       const res = await newRequest.post("/adduser", { name, phone, email });
+      console.log(res.status);
     } catch (error) {
-      setError(error.response.data);
+      console.log(error);
     }
   };
 
@@ -23,11 +25,30 @@ const App = () => {
     <div>
       <h1>User Address Book</h1>
       <div className="flex flex-col gap-3 justify-center items-center">
-        <input ref={nameRef} type="text" placeholder="Name" />
-        <input ref={phoneRef} type="number" placeholder="Phone Number" />
-        <input ref={emailRef} type="email" placeholder="Email" />
+        <input
+          className="text-black"
+          ref={nameRef}
+          onChange={() => setName(nameRef.current.value)}
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          className="text-black"
+          ref={phoneRef}
+          onChange={() => setPhone(phoneRef.current.value)}
+          type="number"
+          placeholder="Phone Number"
+        />
+        <input
+          className="text-black"
+          ref={emailRef}
+          onChange={() => setEmail(emailRef.current.value)}
+          type="email"
+          placeholder="Email"
+        />
         <Button onClick={handleSubmit} value={"Submit"} />
       </div>
+     
     </div>
   );
 };
