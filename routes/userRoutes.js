9efe252199d845d4b6fdb08197ab210f97c674b5
sugeshc389 <sugeshc389@gmail.com>
2024-04-07 04:04:27 +0530

    updated changes

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { addUser } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.post('/adduser',addUser)
-
-export default router;
