7db7c39bb9ad40cb394fbe4f3c5e822899f9cbb5
sugeshc389 <sugeshc389@gmail.com>
2024-04-07 15:12:49 +0530

    fet:edit user

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,3 +22,31 @@ export const getUsers = async (req, res) => {
     console.log(error);
   }
 };
+
+export const editUser = async (req, res) => {
+  try {
+    const findUser = await User.findById(req.body.id);
+    console.log(findUser, "edituser");
+    res.status(201).send(findUser);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const id = req.body.userId;
+    const name = req.body.name;
+    const phone = req.body.phone;
+    const email = req.body.email;
+    console.log(name);
+    const updatedUser = await User.findByIdAndUpdate(
+      { _id: id },
+      { name: name, email: email, phone: phone }
+    );
+    console.log(updatedUser, "This is updated user");
+    res.status(201).send(updatedUser);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { addUser, getUsers } from "../controllers/userController.js";
+import {
+  addUser,
+  editUser,
+  getUsers,
+  updateUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/adduser", addUser);
 router.get("/getusers", getUsers);
+router.put("/edituser", editUser);
+router.put("/updateUser", updateUser);
 
 export default router;
